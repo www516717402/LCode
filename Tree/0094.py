c9567d07829a8c7176b41a59e940e07b8a3feffe
www516717402 <www516717402@gmail.com>
2022-03-26 11:18:49 +0000

    add tree-0094

---
+++ Tree/0094.py
@@ -0,0 +1,26 @@
+from re import X
+
+
+class TreeNode(object):
+    def __init__(self, val) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def inorderTraversal(root):
+    def in_order(root):
+        if not root:
+            return
+        yield from in_order(root.left)
+        yield root.val
+        yield from in_order(root.right)
+
+    return [i for i in in_order(root)]
+
+
+a = TreeNode(1)
+a.right = TreeNode(2)
+a.right.left = TreeNode(3)
+
+print(inorderTraversal(a))
